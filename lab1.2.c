121cc267cbfb22f99afe89cb7f934c8c9b217a38
Chayanon_kh <chayanon5826@gmail.com>
2025-12-04 21:12:43 +0700

    Add files via upload

---
+++ lab1.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void GetSet(int s[], int *n) {
+    printf("Input N: ");
+    scanf("%d", n);
+    for (int i = 0; i < *n; i++) {
+        printf("Number %d: ", i + 1);
+        scanf("%d", &s[i]); 
+    }
+
+}
+int main() {
+    int data[100];
+    int count;   
+    GetSet(data, &count);
+       printf("Output: Set has %d members: ", count);
+    for (int i = 0; i < count; i++)  {
+        printf("%d ", data[i]);
+    
+    }
+    return 0;
+}
