c5adcb6b2316ad39479ea9572519c9cd56ec1bd9
Chayanon_kh <chayanon5826@gmail.com>
2025-12-12 20:22:36 +0700

    Update lab2.2.c

--- lab2.2.c
+++ lab2.2.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 char* reverse(char str1[]) {
-    static char str2[100];   // ใช้ static เพื่อให้ส่ง pointer กลับได้
+    static char str2[100];
     int j = 0;
-
-    // หาความยาว string
     while (str1[j] != '\0') {
         j++;
     }
-
-    // กลับด้านข้อความ
     for (int i = 0; i < j; i++) {
         str2[i] = str1[j - 1 - i];
     }
 
-    str2[j] = '\0'; // ปิดท้าย string
+    str2[j] = '\0';
 
     return str2;
 }
-
 int main() {
     char text[50] = "I Love You";
     char *out;
